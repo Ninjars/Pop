76efe24b99dcc954c721b1ca8caee62f1976bba1
Jez <>
2021-08-02 12:53:32 +0100

    cleanup and simplification

--- app/src/main/java/jez/jetpackpop/model/MutableAppState.kt
+++
@@ -1,9 +0,0 @@
-package jez.jetpackpop.model
-
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
-
-class MutableAppState {
-    var state by mutableStateOf<AppState>(AppState.InitialisingState)
-}
--- app/src/main/java/jez/jetpackpop/ui/App.kt
+++ app/src/main/java/jez/jetpackpop/ui/App.kt
@@ -4,16 +4,13 @@ import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.Stable
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import jez.jetpackpop.model.AppState
 import jez.jetpackpop.model.GameConfiguration
-import jez.jetpackpop.model.MutableAppState
 import jez.jetpackpop.model.TargetConfiguration
 import jez.jetpackpop.ui.components.GameEndMenu
 import jez.jetpackpop.ui.components.GameScreen
@@ -27,62 +24,36 @@ fun App() {
             modifier = Modifier
                 .background(MaterialTheme.colors.background)
         ) {
-//            var gameConfiguration by rememberSaveable { mutableStateOf<GameConfiguration?>(null) }
-//            var isRunning by rememberSaveable { mutableStateOf(false) }
-//            var endGameState by rememberSaveable { mutableStateOf<GameEndState?>(null) }
-            val appState = remember { MutableAppState() }
+            val appState = rememberSaveable { mutableStateOf<AppState>(AppState.InitialisingState) }
 
-            if (appState.state is AppState.InitialisingState) {
-                appState.state = AppState.MainMenuState(demoConfiguration(target1)) {
-                    appState.state = AppState.StartGameState(lvlConfiguration(target1))
+//            when (appState.value) {
+//                AppState.InitialisingState -> {
+//                    appState.value = AppState.MainMenuState(demoConfiguration(target1)) {
+//                        appState.value = AppState.StartGameState(lvlConfiguration(target1))
+//                    }
+//                }
+//                is AppState.MainMenuState -> {
+//                    ShowMainMenu(appState.value)
+//                }
+//                is AppState.StartGameState -> {
+//                    InitialiseGame(appState)
+//                }
+//                is AppState.InGameState -> {
+//                    ShowGame(appState)
+//                }
+//                is AppState.EndMenuState -> {
+//                    ShowEndMenu(appState)
+//                }
+//            }
+            if (appState.value is AppState.InitialisingState) {
+                appState.value = AppState.MainMenuState(demoConfiguration(target1)) {
+                    appState.value = AppState.StartGameState(lvlConfiguration(target1))
                 }
             }
-            ShowMainMenu(appState.state)
+            ShowMainMenu(appState.value)
             InitialiseGame(appState)
             ShowGame(appState)
             ShowEndMenu(appState)
-
-            Log.w("JEZTAG", "recomposing app with state ${appState.state}")
-//            appState.state = when (val currentState = appState.state) {
-//                is AppState.InitialisingState ->
-//                    AppState.MainMenuState(demoConfiguration(target1)) {
-//                        appState.state = AppState.StartGameState(demoConfiguration(target1))
-//                    }
-//                is AppState.MainMenuState -> {
-//                    ShowMainMenu(currentState)
-//                    currentState
-//                }
-//                is AppState.StartGameState -> {
-//                    ShowGame(
-//                        currentState.gameConfiguration,
-//                        isRunning = false,
-//                        reset = true,
-//                    ) {
-//                        appState.state = AppState.EndMenuState(it)
-//                    }
-//                    AppState.InGameState(
-//                        currentState.gameConfiguration,
-//                        isRunning = true,
-//                    ) {
-//                        appState.state = AppState.EndMenuState(it)
-//                    }
-//                }
-//                is AppState.InGameState -> {
-//                    ShowGame(
-//                        currentState.gameConfiguration,
-//                        isRunning = currentState.isRunning,
-//                        reset = false,
-//                        endGameAction = currentState.endGameAction,
-//                    )
-//                    currentState
-//                }
-//                is AppState.EndMenuState -> {
-//                    ShowEndMenu(currentState) {
-//                        appState.state = AppState.StartGameState(demoConfiguration(target1))
-//                    }
-//                    currentState
-//                }
-//            }
         }
     }
 }
@@ -102,22 +73,22 @@ private fun ShowMainMenu(state: AppState) {
 
 @Composable
 fun InitialiseGame(
-    appState: MutableAppState,
+    appState: MutableState<AppState>,
 ) {
-    val state = appState.state
+    val state = appState.value
     if (state is AppState.StartGameState) {
         GameScreen(
             state.gameConfiguration,
             isRunning = false,
             shouldReset = true,
-            gameEndAction = { appState.state = AppState.EndMenuState(it) },
+            gameEndAction = { appState.value = AppState.EndMenuState(it) },
         )
 
-        appState.state = AppState.InGameState(
+        appState.value = AppState.InGameState(
             state.gameConfiguration,
             isRunning = true,
             endGameAction = {
-                appState.state = AppState.EndMenuState(it)
+                appState.value = AppState.EndMenuState(it)
             }
         )
     }
@@ -125,9 +96,9 @@ fun InitialiseGame(
 
 @Composable
 fun ShowGame(
-    appState: MutableAppState,
+    appState: MutableState<AppState>,
 ) {
-    val state = appState.state
+    val state = appState.value
     if (state is AppState.InGameState) {
         GameScreen(
             state.gameConfiguration,
@@ -140,14 +111,14 @@ fun ShowGame(
 
 @Composable
 private fun ShowEndMenu(
-    appState: MutableAppState,
+    appState: MutableState<AppState>,
 ) {
-    val state = appState.state
+    val state = appState.value
     if (state is AppState.EndMenuState) {
         GameEndMenu(
             endState = state.endState,
             startGameAction = {
-                appState.state = AppState.StartGameState(lvlConfiguration(target1))
+                appState.value = AppState.StartGameState(lvlConfiguration(target1))
             }
         )
     }
