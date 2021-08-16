82f13297e52a458986954abb44e6c620f7cf3e9f
Jez <>
2021-08-16 16:55:17 +0100

    removed touch feedback visuals from targets in GameRenderer, as incorrect element was displaying feedback after target was removed

--- app/src/main/java/jez/jetpackpop/ui/components/GameRenderer.kt
+++ app/src/main/java/jez/jetpackpop/ui/components/GameRenderer.kt
@@ -2,11 +2,13 @@ package jez.jetpackpop.ui.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -38,7 +40,10 @@ fun Target(data: TargetData, onClick: (TargetData) -> Unit) {
                 .offset(data.xOffset, data.yOffset)
                 .clip(CircleShape)
                 .background(data.color)
-                .clickable { onClick(data) }
+                .clickable(
+                    interactionSource = remember { MutableInteractionSource() },
+                    indication = null,
+                ) { onClick(data) }
         )
     } else {
         Box(
