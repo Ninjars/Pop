563ae5a10facb1d63506c7bd34e8901653c150d8
Jez <>
2021-08-16 16:54:35 +0100

    simplified end game menu's conditional handling

--- app/src/main/java/jez/jetpackpop/ui/components/GameEndMenu.kt
+++ app/src/main/java/jez/jetpackpop/ui/components/GameEndMenu.kt
@@ -1,5 +1,6 @@
 package jez.jetpackpop.ui.components
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
@@ -9,6 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import jez.jetpackpop.R
@@ -33,45 +35,57 @@ fun GameEndMenu(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.spacedBy(16.dp),
         ) {
-            Box(
-                contentAlignment = Alignment.Center,
-                modifier = Modifier
-                    .background(
-                        color = if (endState.didWin)
-                            MaterialTheme.colors.win
-                        else
-                            MaterialTheme.colors.lose,
-                        shape = CircleShape
-                    )
-                    .fillMaxWidth(0.8f)
-                    .aspectRatio(1f, true)
-            ) {
-                Text(
-                    text = if (endState.didWin)
-                        stringResource(R.string.game_end_win_title)
-                    else
-                        stringResource(R.string.game_end_lose_title),
-                    style = MaterialTheme.typography.h1,
-                    color = MaterialTheme.colors.onEnd,
-                    modifier = Modifier
-                        .wrapContentSize()
-                )
-            }
-            Button(
-                shape = CircleShape,
-                onClick = {
-                    startGameAction(endState)
-                },
-            ) {
-                Text(
-                    text = if (endState.didWin)
-                        stringResource(R.string.game_end_win_start)
-                    else
-                        stringResource(R.string.game_end_lose_start),
-                    style = MaterialTheme.typography.h3,
-                    modifier = Modifier.wrapContentSize()
-                )
+            if (endState.didWin) {
+                DisplayMenu(
+                    color = MaterialTheme.colors.win,
+                    titleText = R.string.game_end_win_title,
+                    buttonText = R.string.game_end_win_start,
+                ) { startGameAction(endState) }
+            } else {
+                DisplayMenu(
+                    color = MaterialTheme.colors.lose,
+                    titleText = R.string.game_end_lose_title,
+                    buttonText = R.string.game_end_lose_start,
+                ) { startGameAction(endState) }
             }
         }
     }
+}
+
+@Composable
+private fun DisplayMenu(
+    color: Color,
+    @StringRes titleText: Int,
+    @StringRes buttonText: Int,
+    startGameAction: () -> Unit,
+) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .background(
+                color = color,
+                shape = CircleShape
+            )
+            .fillMaxWidth(0.8f)
+            .aspectRatio(1f, true)
+    ) {
+        Text(
+            text = stringResource(titleText),
+            style = MaterialTheme.typography.h1,
+            color = MaterialTheme.colors.onEnd,
+            modifier = Modifier
+                .wrapContentSize()
+        )
+    }
+    Button(
+        shape = CircleShape,
+        onClick = { startGameAction() },
+    ) {
+        Text(
+            text = stringResource(buttonText),
+            style = MaterialTheme.typography.h3,
+            modifier = Modifier.wrapContentSize()
+        )
+    }
+
 }
