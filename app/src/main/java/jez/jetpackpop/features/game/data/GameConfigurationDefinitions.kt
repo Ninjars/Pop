b5997f6115bfb43b8f1150fd3146851d666c7110
Jez <>
2021-10-01 20:35:34 +0100

    let's have each chapter the same length

--- app/src/main/java/jez/jetpackpop/features/game/data/GameConfigurationDefinitions.kt
+++ app/src/main/java/jez/jetpackpop/features/game/data/GameConfigurationDefinitions.kt
@@ -1,10 +1,9 @@
 package jez.jetpackpop.features.game.data
 
 import androidx.compose.ui.unit.dp
-import jez.jetpackpop.features.game.data.TargetConfiguration.ClickResult.*
+import jez.jetpackpop.features.game.data.TargetConfiguration.ClickResult.SCORE
 
-private const val SIMPLE_SINGLE_LEVEL_COUNT = 6
-private const val SIMPLE_DECOY_LEVEL_COUNT = 6
+private const val CHAPTER_LEVEL_COUNT = 6
 private const val LEVEL_DURATION = 10f
 
 private fun getProgressionFraction(count: Int, position: Int): Float {
@@ -14,60 +13,56 @@ private fun getProgressionFraction(count: Int, position: Int): Float {
 }
 
 val gameConfigurations = hashMapOf(
-    SIMPLE_SINGLE_LEVEL_COUNT.let { count ->
-        GameChapter.SIMPLE_SINGLE to (0 until count)
-            .map { index ->
-                val progressFraction = getProgressionFraction(
-                    count,
-                    index
-                )
-                GameConfiguration(
-                    id = GameConfigId(GameChapter.SIMPLE_SINGLE, index),
-                    timeLimitSeconds = LEVEL_DURATION,
-                    targetConfigurations = listOf(
-                        TargetConfiguration(
-                            color = TargetColor.TARGET,
-                            radius = (40 - 16 * progressFraction).dp,
-                            count = (6 + 18 * progressFraction).toInt(),
-                            minSpeed = (20 + 60 * progressFraction).dp,
-                            maxSpeed = (30 + 80 * progressFraction).dp,
-                            clickResult = SCORE,
-                        )
+    GameChapter.SIMPLE_SINGLE to (0 until CHAPTER_LEVEL_COUNT)
+        .map { index ->
+            val progressFraction = getProgressionFraction(
+                CHAPTER_LEVEL_COUNT,
+                index
+            )
+            GameConfiguration(
+                id = GameConfigId(GameChapter.SIMPLE_SINGLE, index),
+                timeLimitSeconds = LEVEL_DURATION,
+                targetConfigurations = listOf(
+                    TargetConfiguration(
+                        color = TargetColor.TARGET,
+                        radius = (40 - 16 * progressFraction).dp,
+                        count = (6 + 18 * progressFraction).toInt(),
+                        minSpeed = (20 + 60 * progressFraction).dp,
+                        maxSpeed = (30 + 80 * progressFraction).dp,
+                        clickResult = SCORE,
+                    )
+                ),
+                isLastInChapter = index == CHAPTER_LEVEL_COUNT - 1
+            )
+        },
+    GameChapter.SIMPLE_DECOY to (0 until CHAPTER_LEVEL_COUNT)
+        .map { index ->
+            val progressFraction = getProgressionFraction(
+                CHAPTER_LEVEL_COUNT,
+                index
+            )
+            GameConfiguration(
+                id = GameConfigId(GameChapter.SIMPLE_DECOY, index),
+                timeLimitSeconds = LEVEL_DURATION,
+                targetConfigurations = listOf(
+                    TargetConfiguration(
+                        color = TargetColor.TARGET,
+                        radius = (36 - 12 * progressFraction).dp,
+                        count = (6 + 12 * progressFraction).toInt(),
+                        minSpeed = (30 + 50 * progressFraction).dp,
+                        maxSpeed = (40 + 70 * progressFraction).dp,
+                        clickResult = SCORE,
                     ),
-                    isLastInChapter = index == count - 1
-                )
-            }
-    },
-    SIMPLE_DECOY_LEVEL_COUNT.let { count ->
-        GameChapter.SIMPLE_DECOY to (0 until count)
-            .map { index ->
-                val progressFraction = getProgressionFraction(
-                    count,
-                    index
-                )
-                GameConfiguration(
-                    id = GameConfigId(GameChapter.SIMPLE_DECOY, index),
-                    timeLimitSeconds = LEVEL_DURATION,
-                    targetConfigurations = listOf(
-                        TargetConfiguration(
-                            color = TargetColor.TARGET,
-                            radius = (36 - 12 * progressFraction).dp,
-                            count = (6 + 12 * progressFraction).toInt(),
-                            minSpeed = (30 + 50 * progressFraction).dp,
-                            maxSpeed = (40 + 70 * progressFraction).dp,
-                            clickResult = SCORE,
-                        ),
-                        TargetConfiguration(
-                            color = TargetColor.DECOY,
-                            radius = 80.dp,
-                            count = (2 + 3 * progressFraction).toInt(),
-                            minSpeed = (20 + 30 * progressFraction).dp,
-                            maxSpeed = (25 + 50 * progressFraction).dp,
-                            clickResult = SCORE,
-                        )
-                    ),
-                    isLastInChapter = index == count - 1
-                )
-            }
-    },
+                    TargetConfiguration(
+                        color = TargetColor.DECOY,
+                        radius = 80.dp,
+                        count = (2 + 3 * progressFraction).toInt(),
+                        minSpeed = (20 + 30 * progressFraction).dp,
+                        maxSpeed = (25 + 50 * progressFraction).dp,
+                        clickResult = SCORE,
+                    )
+                ),
+                isLastInChapter = index == CHAPTER_LEVEL_COUNT - 1
+            )
+        },
 )
