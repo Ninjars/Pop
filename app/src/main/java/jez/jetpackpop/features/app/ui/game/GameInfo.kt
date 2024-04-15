9b6fc3ae038de57d1aa5034cc4ee577a0b7bd984
Jez <5053926+ninjars@users.noreply.github.com>
2024-04-15 17:10:38 +0100

    Improved multiplier readout component

--- app/src/main/java/jez/jetpackpop/features/app/ui/game/GameInfo.kt
+++ app/src/main/java/jez/jetpackpop/features/app/ui/game/GameInfo.kt
@@ -1,22 +1,27 @@
 package jez.jetpackpop.features.app.ui.game
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.BiasAlignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import jez.jetpackpop.features.app.model.game.GameScoreData
 import kotlin.math.ceil
 
@@ -32,9 +37,8 @@ fun GameInfo(
             .padding(8.dp)
             .fillMaxWidth()
     ) {
-        ScoreReadout(
-            gameScoreData = scoreData,
-        )
+        Multiplier(scoreData.currentMultiplier)
+        ScoreReadout(scoreData.totalScore)
         CountdownTimer(
             remainingSeconds = ceil(remainingTime).toInt(),
         )
@@ -51,44 +55,61 @@ private fun CountdownTimer(
         Icon(
             imageVector = GameTimer,
             contentDescription = null,
+            tint = MaterialTheme.colors.primary,
             modifier = Modifier
-                .size(60.dp)
+                .size(85.dp)
         )
         AnimatedCounter(
             count = remainingSeconds,
             style = MaterialTheme.typography.h3,
             color = MaterialTheme.colors.onPrimary,
-            fontSize = 30.sp,
             modifier = Modifier
-                .align(BiasAlignment(0f, 1.8f))
+                .align(BiasAlignment(0f, 0.3f))
         )
     }
 }
 
 @Composable
 private fun ScoreReadout(
-    gameScoreData: GameScoreData,
-    modifier: Modifier = Modifier,
+    totalScore: Int,
 ) {
-    Row(modifier = modifier) {
-        Text(
-            text = gameScoreData.totalScore.toString(),
-            style = MaterialTheme.typography.h3,
-            color = MaterialTheme.colors.onSurface,
-        )
-        Text(
-            text = "x",
-            style = MaterialTheme.typography.h3,
-            color = MaterialTheme.colors.onSurface,
-            modifier = Modifier
-                .wrapContentSize()
-                .padding(8.dp, 0.dp)
-        )
-        Text(
-            text = gameScoreData.currentMultiplier.toString(),
-            style = MaterialTheme.typography.h3,
-            color = MaterialTheme.colors.onSurface,
-        )
+    Text(
+        text = totalScore.toString(),
+        style = MaterialTheme.typography.h3,
+        color = MaterialTheme.colors.onSurface,
+    )
+}
+
+@Composable
+private fun Multiplier(
+    multiplier: Int
+) {
+    var oldValue by remember { mutableIntStateOf(multiplier) }
+    SideEffect {
+        oldValue = multiplier
+    }
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .size(70.dp)
+            .background(
+                color = MaterialTheme.colors.primary,
+                shape = CircleShape,
+            )
+    ) {
+        Row {
+            Text(
+                text = "x",
+                style = MaterialTheme.typography.h3,
+                color = MaterialTheme.colors.onPrimary,
+                modifier = Modifier
+            )
+            AnimatedCounter(
+                count = multiplier,
+                style = MaterialTheme.typography.h3,
+                color = MaterialTheme.colors.onPrimary,
+            )
+        }
     }
 }
 
@@ -96,7 +117,7 @@ private fun ScoreReadout(
 @Composable
 private fun GameInfoPreview() {
     GameInfo(
-        remainingTime = 5f,
+        remainingTime = 10f,
         scoreData = GameScoreData(
             startingScore = 10,
             tapHistory = emptyList(),
