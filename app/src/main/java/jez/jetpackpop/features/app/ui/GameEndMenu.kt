d56f1eb4affb68a92a01e7f0a137bf45b8635612
jez <5053926+Ninjars@users.noreply.github.com>
2024-05-04 14:06:18 +0100

    GameEndMenu imports

--- app/src/main/java/jez/jetpackpop/features/app/ui/GameEndMenu.kt
+++ app/src/main/java/jez/jetpackpop/features/app/ui/GameEndMenu.kt
@@ -1,22 +1,17 @@
 package jez.jetpackpop.features.app.ui
 
 import androidx.annotation.StringRes
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import jez.jetpackpop.R
 import jez.jetpackpop.audio.GameSoundEffect
 import jez.jetpackpop.audio.SoundManager
 import jez.jetpackpop.ui.PopMegaButton
+import jez.jetpackpop.ui.ScreenScaffold
 import jez.jetpackpop.ui.lose
 import jez.jetpackpop.ui.win
 
